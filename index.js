133b9799d2bda2f7c1aed1d20920d7c2e4060282
Luis <luismarcelino.dev@gmail.com>
2023-02-14 12:33:10 -0300

    added Post

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = require('express')();
 // Set the port
 const PORT = 8080;
 
+// Starts the server
 app.listen(
     PORT,
     () => console.log('ON on port {http://localhost:8080}')
 )
 
 
-
+// GET 
 app.get('/tshirt', (req, res) =>{
     res.status(200).send({
         tshirt : "Red",
@@ -19,3 +20,17 @@ app.get('/tshirt', (req, res) =>{
     })
 });
 
+// POST
+app.post('/tshirt/:id',(req, res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({message: "We need a logo!"})
+    }
+
+    res.send({
+        tshirt: "Your ${logo} and ID of ${id}",
+    });
+});
